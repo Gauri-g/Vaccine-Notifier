889102026ddf381558eee296f546d90567b44e13
Gauri-g <gaurigupta2727@gmail.com>
2021-05-07 15:20:22 +0530

    Autocomplete component

--- vaccine-notifier/src/components/AutoComplete/Autocomplete.js
+++ vaccine-notifier/src/components/AutoComplete/Autocomplete.js
@@ -1,77 +1,88 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import userEvent from '@testing-library/user-event';
+import React, { useEffect, useState,Component } from 'react';
 import cookie from "react-cookies";
 
-export class Autocomplete extends Component {
-  static propTypes = {
-    options: PropTypes.instanceOf(Array).isRequired
-  };
-  state = {
-    activeOption: 0,
-    filteredOptions: [],
-    showOptions: false,
-    userInput: ''
-  };
+const Autocomplete =({options}) =>
+{
+  // state = {
+  //   activeOption: 0,
+  //   filteredOptions: [],
+  //   showOptions: false,
+  //   userInput: ''
+  // };
+  // let options=[
+  //   "Dehradun",
+  //   "Kolkata",
+  //   "Delhi",
+  //   "Mumbai",
+  //   "Banglore",
+  //   "Vellore",
+  //   "Agra"
+  // ]
+  const [activeOption, setactiveOption] = useState(0);
+  const [filteredOptions, setfilteredOptions] = useState([]);
+  const [showOptions, setshowOptions] = useState(false);
+  const [userInput, setuserInput] = useState('');
 
-  onChange = (e) => {
+  function onChange (e)  {
     console.log('onChanges');
 
-    const { options } = this.props;
-    const userInput = e.currentTarget.value;
-
-    const filteredOptions = options.filter(
+    // const { options } = props;
+    setuserInput(e.target.value);
+    setfilteredOptions (options.filter(
       (optionName) =>
         optionName.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-    );
-
-    this.setState({
-      activeOption: 0,
-      filteredOptions,
-      showOptions: true,
-      userInput: e.currentTarget.value
-    });
+    ));
+      setactiveOption(0);
+      setshowOptions(true);
+      setuserInput(e.target.value);
   };
 
-  onClick = (e) => {
-    this.setState({
-      activeOption: 0,
-      filteredOptions: [],
-      showOptions: false,
-      userInput: e.currentTarget.innerText
-    });
-    cookie.save("firebasecity", e.currentTarget.innerText, { path: "/" });
-  };
-  onKeyDown = (e) => {
-    const { activeOption, filteredOptions } = this.state;
+  useEffect(() => { 
+      setactiveOption(0);
+      setshowOptions(false);
+      setfilteredOptions([]);
+      setuserInput(activeOption.innerText)  
+    cookie.save("firebasecity", activeOption.innerText, { path: "/" });
+  },[userInput]);
 
+  // onClick = (e) => {
+     
+  //   setactiveOption(0),
+  //     setshowOptions(false),
+  //     setfilteredOptions([])
+  //     setuserInput(e.currentTarget.innerText)  
+  
+  //   cookie.save("firebasecity", e.currentTarget.innerText, { path: "/" });
+  // };
+  function onKeyDown (e) {
+    // const { activeOption, filteredOptions } = this.state;
     if (e.keyCode === 13) {
-      this.setState({
-        activeOption: 0,
-        showOptions: false,
-        userInput: filteredOptions[activeOption]
-      });
+      setactiveOption(0);
+      setshowOptions(false);
+      setuserInput(filteredOptions[activeOption]);
     } else if (e.keyCode === 38) {
       if (activeOption === 0) {
         return;
       }
-      this.setState({ activeOption: activeOption - 1 });
+      setactiveOption(activeOption - 1 );
     } else if (e.keyCode === 40) {
       if (activeOption === filteredOptions.length - 1) {
         console.log(activeOption);
         return;
       }
-      this.setState({ activeOption: activeOption + 1 });
+      setactiveOption(activeOption + 1 );
     }
   };
 
-  render() {
-    const {
-      onChange,
-      onClick,
-      onKeyDown,
+  // render() {
+  //   const {
+  //     onChange,
+  //     onClick,
+  //     onKeyDown,
 
-      state: { activeOption, filteredOptions, showOptions, userInput }
-    } = this;
+  //     state: { activeOption, filteredOptions, showOptions, userInput }
+  //   } = this;
     let optionList;
     if (showOptions && userInput) {
       if (filteredOptions.length) {
@@ -83,7 +94,7 @@ export class Autocomplete extends Component {
                 className = 'option-active';
               }
               return (
-                <li className={className} key={optionName} onClick={onClick}>
+                <li className={className} key={optionName} >
                   {optionName}
                 </li>
               );
@@ -114,6 +125,6 @@ export class Autocomplete extends Component {
       </React.Fragment>
     );
   }
-}
+
 
 export default Autocomplete;
