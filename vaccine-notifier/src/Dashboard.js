719d32aa833f72a7d6beee1c965e45287df0b218
Gauri-g <gaurigupta2727@gmail.com>
2021-05-07 10:42:20 +0530

    feat : Form created

--- vaccine-notifier/src/Dashboard.js
+++ vaccine-notifier/src/Dashboard.js
@@ -1,29 +1,87 @@
 import React from "react";
 import "./Dashboard.css";
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext, useState,useRef } from "react";
 import { UserContext } from "./providers/UserProvider";
 import { Redirect } from "react-router-dom";
 import { logOut } from "./services/firebase";
-import firebase from "firebase/app"; 
-import cookie,{removeCookie} from "react-cookies"; 
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import cookie, { removeCookie } from "react-cookies";
+import "firebase/firestore";
 require("firebase/auth");
 
 export default function Dashboard() {
-  const user = useContext(UserContext);
-  const [redirect, setredirect] = useState(null); 
+  const cityRef = useRef("");
+  const [selectedOption, setselectedOption] = useState("");
+   
 
-  
+   function onValueChange(event){
+     setselectedOption(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const uid = cookie.load("firebaseUid");
+    const db = firebase.firestore();
+ 
+    db.collection("users").doc(uid).update({
+      city: cityRef.current.value,
+      age: selectedOption,
+      })
+      .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+  }
+
   return (
-    <div className="dashboard">
-      <h1 className="dashboard-text">Welcome Home</h1>
-      <button className="logout-button" onClick={logOut}>
-        <img
-          src="https://img.icons8.com/ios-filled/50/000000/google-logo.png"
-          alt="google icon"
-        />
-        <span> logout</span>
-      </button>
-    </div>
+    <>
+      <div>
+        <form onSubmit={submitHandler}>
+          <div>
+          <div className="radio">
+          <label>
+            <input
+              type="radio"
+              value="Below45"
+              checked={ selectedOption === "Below45"}
+              onChange={onValueChange}
+            />
+            Below 45
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input
+              type="radio"
+              value="Above45"
+              checked={ selectedOption === "Above45"}
+              onChange={onValueChange}
+            />
+            Above 45
+          </label>
+        </div>
+            <input
+              type="text"
+              id="city"
+              ref={cityRef}
+              placeholder="City"
+            />
+          </div>
+          <button> Submit </button>
+        </form>
+      </div>
+      <div className="dashboard">
+        <h1 className="dashboard-text">Welcome Home</h1>
+        <button className="logout-button" onClick={logOut}>
+          <img
+            src="https://img.icons8.com/ios-filled/50/000000/google-logo.png"
+            alt="google icon"
+          />
+          <span> logout</span>
+        </button>
+      </div>
+    </>
   );
 }
--- vaccine-notifier/src/services/firebase.js
+++ vaccine-notifier/src/services/firebase.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
-import firebase from "firebase/app"; 
-import cookie,{removeCookie} from "react-cookies"; 
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import cookie, { removeCookie } from "react-cookies";
+import "firebase/firestore";
 require("firebase/auth");
 
 dotenv.config();
@@ -22,42 +22,33 @@ console.log(process.env.REACT_APP_API_KEY);
 export const auth = firebase.auth();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-
 export const signInWithGoogle = () => {
   auth
     .signInWithPopup(googleProvider)
+
     .then((res) => {
-      cookie.save("key",res.credential.idToken,{path: "/"})
+      cookie.save("key", res.credential.idToken, { path: "/" });
       const db = firebase.firestore();
-      db.collection("users").doc(res.user.uid).set({
-        name: JSON.stringify(res.user.displayName),
-        email: JSON.stringify(res.user.email),
-    })
-    cookie.save("firebaseUid",res.user.uid,{path: "/"})
-    window.location.href="/dashboard";
-    console.log(res);
-    // console.log(res.user_id)
-    // console.log(res.user.displayName)
-    // console.log(res.user.email)
-    // .then(() => {
-    //     console.log("Document successfully written!");
-    // })
-    // .catch((error) => {
-    //     console.error("Error writing document: ", error);
-    // });
+      db.collection("users")
+        .doc(res.user.uid)
+        .set({
+          name: JSON.stringify(res.user.displayName),
+          email: JSON.stringify(res.user.email),
+        });
+
+      cookie.save("firebaseUid", res.user.uid, { path: "/" });
+      window.location.href = "/dashboard";
+      console.log(res);
     })
     .catch((error) => {
       console.log(error.message);
     });
 };
 
-
 export const logOut = () => {
   cookie.remove("key");
-  window.location.reload(); 
+  window.location.reload();
 };
-
-
 
 /*
   Storing the idtoken in the key 
